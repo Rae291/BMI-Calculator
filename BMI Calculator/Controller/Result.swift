24ad1ae20b3bd379a0ec7121bed09aa86ad61daa
Rae291 <107467793+Rae291@users.noreply.github.com>
2022-12-24 18:26:19 +0530

    Add files via upload

---
+++ BMI Calculator/Controller/Result.swift
@@ -0,0 +1,8 @@
+//
+//  Result.swift
+//  BMI Calculator
+//
+//  Created by APPLE on 19/12/22.
+//
+
+import Foundation
---
+++ BMI Calculator/Model/BMI.swift
@@ -0,0 +1,15 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by APPLE on 19/12/22.
+//
+
+import UIKit
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
+
